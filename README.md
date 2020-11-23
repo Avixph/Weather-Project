64b1a0662b20382257bf5299598bd698a242e01f
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-11-22 23:58:08 -0500

    second commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![storm.ly_logo](images/storm.ly_logo.png)
+![storm.ly_logo](images+icons/storm.ly_logo.png)
 # Weather-Project
 > - This is where you will be completing and pull requesting your project
 > - You have to fork and clone this repository
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
 const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+const weatherUrl = process.env.BASE_URL;
 //remember, make a .env file and save your key
-const api_key = 'Your key here';
+const api_key = process.env.API_KEY;
