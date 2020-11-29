242ed7d3dfdf1f3dabb96c7094fe25f63e0e0e1d
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-11-29 06:28:25 -0500

    complete

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
+// const { default: Axios } = require("axios");
+
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const defaultLocationInfo = "New York, US";
-document.onload = paint(defaultLocationInfo);
-
 const searchInput = document.querySelector("#city-search");
-const getTempButton = document.querySelector("#getTemp");
+const getWeather = document.querySelector("#getTemp");
 
 const locationInfo = document.querySelector(".location p");
 const iconInfo = document.querySelector(".weather-icon");
@@ -20,82 +19,43 @@ const api_key = process.env.API_KEY;
 // const weatherInfo = {};
 
 //Add event listener to search button
-getTempButton.addEventListener("click", async (e) => {
-  e.preventDefault();
-  getWeatherData(searchInput.value);
-});
+getWeather.addEventListener("click", async () => {
+  let locationQuery = searchInput.value;
+  try {
+    // Get weather data from api
+    let weatherApi = await axios.get(
+      `${weatherUrl}2.5/weather?q=${locationQuery}&units=imperial&APPID=${api_key}`
+    );
+    console.log(weatherApi);
 
-// Get weather data from api
-function getWeatherData(query) {
-  let api = `${weatherUrl}2.5/weather?q=${query}&units=imperial&APPID=${api_key}`;
+    // Display the weather to the UI
+    let locationValue = `${weather.name}, ${weather.sys.country}`;
+    locationInfo.appendChild(locationValue);
 
-  fetch(api)
-    .then((weather) => errorHandler(weather))
-    .then((weather) => {
-      return weather.json();
-    })
-    // .then(function (data) {
-    //   weatherInfo.city = weather.name;
-    //   weatherInfo.country = weather.sys.country;
-    //   weatherInfo.iconId = weather.weather[0].icon;
-    //   weatherInfo.description = weather.weather[0].description;
-    //   weatherInfo.temperature.value = Math.floor(weather.main.temp);
-    //   weatherInfo.high.value = Math.floor(weather.main.temp_max);
-    //   weatherInfo.low.value = Math.floor(weather.main.temp_min);
-    // })
-    .then(displayWeatherData)
-    .catch((error) => console.log(error));
-}
+    let iconnValue = `<img src="images+icons/${weather.weather[0].icon}.png"/>`;
+    iconInfo.appendChild(iconnValue);
 
-// Display the weather to the UI
-function displayWeatherData(weather) {
-  let locationValue = `${weather.name}, ${weather.sys.country}`;
-  locationInfo.appendChild(locationValue);
+    let descValue = weather.weather[0].description;
+    descInfo.appendChild(descValue);
 
-  let iconnValue = `<img src="images+icons/${weather.weather[0].icon}.png"/>`;
-  iconInfo.appendChild(iconnValue);
+    let tempValue = `${Math.floor(weather.main.temp)}°<span>F</span>`;
+    tempInfo.appendChild(tempValue);
 
-  let descValue = weather.weather[0].description;
-  descInfo.appendChild(descValue);
+    let highLowValue = `H:${Math.floor(
+      weather.main.temp_max
+    )}<span>°F</span> L:${Math.floor(weather.main.temp_min)}<span>°F</span>`;
+    highLowInfo.appendChild(highLowValue);
 
-  let tempValue = `${Math.floor(weather.main.temp)}°<span>F</span>`;
-  tempInfo.appendChild(tempValue);
-
-  let highLowValue = `H:${Math.floor(
-    weather.main.temp_max
-  )}<span>°F</span> L:${Math.floor(weather.main.temp_min)}<span>°F</span>`;
-  highLowInfo.appendChild(highLowValue);
-
-  store(locationValue);
-}
-
-function store(location) {
-  localStorage.setItem("location", location);
-}
-
-//Changing the default location to searched one
-function paint(defaultLocationInfo) {
-  if (localStorage.getItem("location") === null) {
-    getResults(defaultLocationInfo);
-  } else {
-    getResults(localStorage.getItem("location"));
-  }
-}
-
-toggler.addEventListener("change", convertCurrent);
-function convertCurrent(event) {
-  // currentCity = document.querySelector(".location .city").innerText;
-  getResults(locationInfo);
-}
-
-//Error handling
-function errorHandler(weather) {
-  if (!weather.ok) {
-    if (weather.status === 404) {
-      throw new Error("City Not Found");
-    } else {
-      throw new Error(weather.status + " : " + weather.statusText);
+    //check if the temperature is under 40, and red if above 90
+    if (Math.floor(weather.main.temp) <= 40) {
+      tempInfo.getElementsByClassName.color = "var(--third-color)";
+    } else if (Math.floor(weather.main.temp) >= 90) {
+      tempInfo.getElementsByClassName.color = "var(--sixth-color)";
     }
+  } catch (err) {
+    //Error handling
+    console.log("Incorrect city name.", err);
+    let errorMessage = `Error: City Not Found.`;
+    locationInfo.appendChild(errorMessage);
   }
-  return weather;
-}
+});
